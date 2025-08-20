935367c8820441eeb29d5ae4764ae5385ac98f4e
dogo1017 <douglas.london@ucas-edu.net>
2025-08-20 18:11:55 +0000

    removed extra lines

--- practices/hello_world.py
+++ practices/hello_world.py
@@ -3,8 +3,6 @@ returning_users = ["Bob", "Joe", "Tim", "Alan", "Shrek"]
 name = input("What is your name: ")
 if name == "Douglas":
      print("Hello awesome person")
-elif name == "Jacob":
-     print("Oh... It's you")
 elif name == "Ms. LaRose":
      print("please give me an A", name)
 elif name in returning_users:
