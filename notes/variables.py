583e9226dcc7e935582165951a23f77eae30df12
dogo1017 <douglas.london@ucas-edu.net>
2025-08-21 09:52:21 -0600

    Variables notes

---
+++ notes/variables.py
@@ -0,0 +1,9 @@
+# DL 1st, variables,py
+import time
+import os
+even_number = 2
+grade = 99
+animal = "panda"
+print(grade)
+grade = 95
+print(grade)
---
+++ starters/seasons.py
@@ -0,0 +1,5 @@
+# Douglas London 1st Seasons
+
+last_name = input("What is your last name: ")
+season = input("What is your favorite season: ")
+print("Wow", last_name, "san, I like", season, "too!")
