c566a8c9b758ae4175598a2361c8c82562295a82
dogo1017 <douglas.london@ucas-edu.net>
2025-08-27 09:52:33 -0600

    int and floats notes

---
+++ notes/numbers.py
@@ -0,0 +1,29 @@
+# DL 1st, Integers and Floats Notes
+
+#Integers
+age = 99999999999999999999
+year = 2015
+room = 200
+hour = 9
+
+#Float (FLoating point number)
+pi = 1.14
+percent = .75
+damage = 9.75
+
+#type casting
+num = int(3.14) # does not round
+print(num)
+decimal = float(8)
+print(decimal)
+
+
+#Using variables
+hours = 3
+minutes = 4
+
+print(hours*60+minutes)
+
+age = int(input("How old are you: \n"))
+
+print("Wow you are old, that is ", 525600*age, "minutes, or", 60*525600*age, "seconds old!")
