1757d167a565cb2f339edfaf749cdfc7c069d93f
dogo1017 <douglas.london@ucas-edu.net>
2025-09-02 09:52:58 -0600

    strings notes

---
+++ notes/strings.py
@@ -0,0 +1,26 @@
+# DL 1st, strings notes
+
+# examples
+
+sentence = "The quick brown fox jumps over the lazy dog!"
+
+first_name = "Tia"
+month = "September"
+book = "The Return of the King"
+food = "chocolate"
+
+print(sentence)
+length = len(sentence)
+print(length)
+
+print("\"Inkheart\" is the best book ever!")
+
+last_name = "Larose"
+full_name = first_name + " " + last_name
+
+print(full_name)
+
+print(last_name[:2])
+print(last_name[2:])
+print(last_name[2:-2])
+print(sentence[10:15])
