f4730f5ff38a1fb5b24f26b53e62cd6916f0c67d
dogo1017 <douglas.london@ucas-edu.net>
2025-09-05 09:52:55 -0600

    started idiot proofing

--- practices/idiot_proof.py
+++ practices/idiot_proof.py
@@ -1,3 +1,34 @@
 #DL 1st, idiot proof
 
-name = name
+def validate_name(name):
+    name = name.strip().lower()
+    if len(name) == 0:
+        return False
+    if name.count(" ") > 2 or name.count(" ") < 1:
+        return False
+    first_name, last_name = name.split(" ")
+    if len(first_name) or len(last_name) == 0:
+        return False
+    name = first_name.capitalize() + " " + last_name.capitalize()
+    return name
+
+
+while True:
+    name = input("What is your full name (First and Last): ")
+    if not validate_name(name) = False
+        name = validate_name(name)
+        print(name)
+        print(validate_name(name))
+        break
+    else:
+        print("Please write a valid name")
+
+
+name = input("What is your full name: ").strip().title
+phone = input("What is your phone number : ")
+GPA = input("What is your GPA: ")
+
+
+
+
+print(f"name: {name}\n\nphone: {phone}\n\nGPA: {GPA}")
