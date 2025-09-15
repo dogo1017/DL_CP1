9c22b6091b9a55ac46233ae028d08c59968ce231
dogo1017 <douglas.london@ucas-edu.net>
2025-09-15 09:49:07 -0600

    finished crew shares

--- project/crew_shares.py
+++ project/crew_shares.py
@@ -1,12 +1,34 @@
 #DL 1st, crew shares project
 
+# story
 print("The crew earned a whole bunch of money on the last outing (an input in dollars), but the captain didn't have time to divvy it all up before release everyone to port. He gave each member of the crew 500 dollars for the evening and then sat down with his first mate to properly divide the shares.")
 
-members_of_crew = input("How many crew members were there (not including first mate and captain): ")
-amount_earned = input("How much was earned total")
+# inputs from user
+members_of_crew = int(input("How many crew members were there (not including first mate and captain): ").strip().replace(",", ""))
+amount_earned = float(input("How much was earned total: ").strip().replace(",", ""))
 
-captain_share = 0.7
-first_mate_share = 0.3
-crew_shares = 0.1
-deduction = 500
+# number of shares each group gets
+captain_share = 7 
+first_mate_share = 3 
+crew_shares = 1
 
+deduction = 500 # amount of money the captain already paid
+shares = amount_earned / (members_of_crew + captain_share + first_mate_share) # amount of money in each share
+
+# total amounts earned
+captain_earned = captain_share * shares
+first_mate_earned = first_mate_share * shares
+crew_earned = (crew_shares * shares) -deduction
+
+# rounding
+amount_earned = round(amount_earned, 2)
+captain_earned = round(captain_earned, 2)
+first_mate_earned = round(first_mate_earned, 2)
+crew_earned = round(crew_earned, 2)
+
+# outputs
+print(f"How much was earned: {amount_earned:.2f}\n")
+print(f"How many crew members are there (not including the captain and first mate): {members_of_crew}\n")
+print(f"The captain gets: ${captain_earned:.2f}\n")
+print(f"The first mate gets: ${first_mate_earned:.2f}\n")
+print(f"Crew still needs: ${crew_earned:.2f}\n")
