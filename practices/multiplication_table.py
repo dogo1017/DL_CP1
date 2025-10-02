93ab4ffc229a330f8d7f9b3b2b545927214f5b5f
dogo1017 <douglas.london@ucas-edu.net>
2025-10-02 09:33:54 -0600

    finished multiplication table

--- practices/multiplication_table.py
+++ practices/multiplication_table.py
@@ -1,2 +1,14 @@
 #DL 1st, multiplication table practice
 
+rows = [1,2,3,4,5,6,7,8,9,10,11,12]
+columns = [1,2,3,4,5,6,7,8,9,10,11,12]
+column_values = []
+
+for row in rows:
+    for column in columns:
+        column_values.append(column*row)
+    print(*column_values)
+    column_values.clear()
+
+
+
