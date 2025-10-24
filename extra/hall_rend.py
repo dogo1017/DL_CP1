03f9d8376998ec85c93ec3bd582d95dd923cd9f1
dogo1017 <douglas.london@ucas-edu.net>
2025-10-24 14:58:03 +0000

    stuff

--- extra/hall_rend.py
+++ extra/hall_rend.py
@@ -13,22 +13,22 @@ hall4 = """
      #  #  #       #  #  #         
      #  #             #  #              
      #                   #           
-   #                       #
+   #                       # 
  """
 
 hall3 = """
- #                             #
-    #                      #    
-       #                #  #          
-       #              #    #        
-        #  #       #  #    # 
-        #    #####    #    #   
-        #    #####    #    #
-        #    #####    #    #        
-        #  #       #  #    #         
-       #               #   #              
-    #                    # #          
- #                         #
+ #                               #\n
+    #                       #\n    
+    #  #                 #  #\n          
+    #    #             #    #\n        
+    #    #  #       #  #    #\n 
+    #    #    #####    #    #\n   
+    #    #    #####    #    #\n
+    #    #    #####    #    #\n        
+    #    #  #       #  #    #\n         
+    #   #               #   #\n              
+    # #                   # #\n          
+   #                         #\n
  """
 
 hall2 = """
@@ -61,11 +61,19 @@ hall1 = """
       
 
 """
+                                          
+   
 
 
 
 
 print(hall1)
 time.sleep(3)
-os.system('cls')
+os.system('clear')
 print(hall2)
+time.sleep(3)
+os.system('clear')
+print(hall3)
+time.sleep(3)
+os.system('clear')
+print(hall4)
