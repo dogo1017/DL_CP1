3842bbe183a97346a7291f1710503bb089fbb8b8
dogo1017 <douglas.london@ucas-edu.net>
2025-10-29 14:57:50 +0000

    turtle stuff

--- practices/turtle_race.py
+++ practices/turtle_race.py
@@ -4,4 +4,10 @@ import turtle as t
 import random
 
 def map_set():
+    t.clearscreen()
+    t.penup()
+    t.teleport() # chose screen size
+    t.pencolor("black")
+    t.down() # chose neg of previous telport
+    t.penup()
     
