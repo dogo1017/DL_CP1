50755cf2c12ca257b2819c52064398b3a0e9b9e0
dogo1017 <douglas.london@ucas-edu.net>
2025-10-30 09:53:11 -0600

    almost finished turtle race

--- practices/turtle_race.py
+++ practices/turtle_race.py
@@ -1,47 +1,66 @@
 #DL 1st, turtle race practice
 
-import turtle as t
+from turtle import *
 import random
 
+height = window_height()
+width = window_width()
+screen = Screen()
+screen_width = window_width()
+screen_height = window_height()
+
+def win(cur_pos):
+    finish_line_x = screen_width // 2 - 50
+    return cur_pos >= finish_line_x
+
+
 def steps():
-    steps = random.randint(10,50)
+    steps = random.randint(100,500)
     return steps
 
-screen = t.Screen()
-screen.setup(600,400)
+def map_setup():
+    speed(10)
+    penup()
+    teleport((width/10*4),(height/3))
+    pendown()
+    right(90)
+    forward(height-(height/3))
+    speed(1)
+    hideturtle()
 
 
-map = t.Turtle()
-map.teleport(580,380) 
-map.pencolor("black")
-map.pendown()
-map.right(90)
-map.forward(360)
-map.penup()
-    
+map_setup()
 
 
-t1 = t.Turtle()
+t1 = Turtle()
 t1.color("red")
 t1.shape("turtle")
-t2 = t.Turtle()
+t1.teleport((width/1000 - 600),(height/10-300))
+t2 = Turtle()
 t2.color("yellow")
 t2.shape("turtle")
-t3 = t.Turtle()
+t2.teleport((width/1000 - 600),(height/10*2-300))
+t3 = Turtle()
 t3.color("blue")
 t3.shape("turtle")
-t4 = t.Turtle()
+t3.teleport((width/1000 - 600),(height/10*3-300))
+t4 = Turtle()
 t4.color("green")
 t4.shape("turtle")
-t5 = t.Turtle()
+t4.teleport((width/1000 - 600),(height/10*4-300))
+t5 = Turtle()
 t5.color("purple")
 t5.shape("turtle")
+t5.teleport((width/1000 - 600),(height/10*5-300))
 
 all_turtles = [t1, t2, t3, t4, t5]
 
 while True:
-    for turtle in all_turtles:
-        print("hi")
-        break
-
-t.done()
+    for all_turtle in all_turtles:
+        all_turtle.forward(steps())
+        if all_turtle.xcor() > 250:
+            race_is_on = False
+            winning_color = all_turtle.pencolor()
+            print(f"The {winning_color} turtle wins!")
+            done()
+            break
