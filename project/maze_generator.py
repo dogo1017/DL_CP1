679f8556bbfae5f96455bf3648fbf2b831e2eb22
dogo1017 <douglas.london@ucas-edu.net>
2025-10-31 09:52:31 -0600

    started maze generator

---
+++ project/maze_generator.py
@@ -0,0 +1,29 @@
+#DL 1st, maze_generator
+
+import random
+import turtle as t
+
+"WWWWWWWWWWWWWWWWWWWWWWWWWWEWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"W#####################################################W"
+"WWWWWWWWWWWWWWWWWWWWWWWWWWXWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+
+
+# check if i of item below is open and next to it and i of item above if they are all walls then change current square to F and move onto an H square and check again using continue
+
+# set current square its on to check the list for open spots and choose a random one, then move to that square and set the previous square to H
