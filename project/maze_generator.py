60c4e107f88e11bb917c67d17c6b71618a022a3b
dogo1017 <douglas.london@ucas-edu.net>
2025-11-03 09:53:12 -0700

    started maze gen movement

--- project/maze_generator.py
+++ project/maze_generator.py
@@ -1,27 +1,59 @@
-#DL 1st, maze_generator
+# DL 1st, maze_generator
 
 import random
 import turtle as t
 
-"WWWWWWWWWWWWWWWWWWWWWWWWWWEWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"W#####################################################W"
-"WWWWWWWWWWWWWWWWWWWWWWWWWWXWWWWWWWWWWWWWWWWWWWWWWWWWWWW"
+maze_temp = [
+"WOWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+"WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWXW"
+]
+
+
+
+
+vert_move_opts = []
+horz_move_opts = []
+cur_pos = [0,2,2]
+
+while True:
+    down_check = maze_temp[cur_pos[1]-2]
+    up_check = maze_temp[cur_pos[1]+2]
+    if cur_pos[2] + 2 == "W":
+        vert_move_opts.append(cur_pos[1])
+        horz_move_opts.append(cur_pos[2] + 2)
+    if cur_pos[2] - 2 == "W":
+        vert_move_opts.append(cur_pos[1])
+        horz_move_opts.append(cur_pos[2] - 2)
+    if down_check[cur_pos[2]] == "W":
+        vert_move_opts.append(cur_pos[1]-2)
+        horz_move_opts.append(cur_pos[2])
+    if up_check[cur_pos[2]] == "W":
+        vert_move_opts.append(cur_pos[1]+2)
+        horz_move_opts.append(cur_pos[2])
+    break
+
+print(vert_move_opts)
+print(horz_move_opts)
+
+
+
 
 
 # check if i of item below is open and next to it and i of item above if they are all walls then change current square to F and move onto an H square and check again using continue
