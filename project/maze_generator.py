93888fbdb500bf1a632d0f058bed7f7e16bc8702
dogo1017 <douglas.london@ucas-edu.net>
2025-11-05 09:53:08 -0700

    started checking for solvability

--- project/maze_generator.py
+++ project/maze_generator.py
@@ -3,14 +3,37 @@
 import random
 import turtle as t
 
-def is_solvable(grid):
+def is_solvable(grid_rows):
     
-    stack_unvisited = True
-    stack = [0,0]
+    stack_unvisited = False
+    stack = [[0,0]]
     x = 0
     y = 0
+    stack_pos = 0
+    visited = []
     while stack_unvisited == False:
-        
+        if  [len(grid_rows[1]),len(grid_rows)] in stack:
+            return True
+        x = stack[stack_pos][0]
+        y = stack[stack_pos][1]
+        if stack[stack_pos] in visited:
+            stack_pos += 1
+            continue
+        visited.append(stack[stack_pos])
+        if grid_rows[x+1][y] >= 0 and grid_rows[x+1][y] <= len(grid_rows[1]) and grid_rows[x+1][y] == 0:
+            stack.append([x+1, y])
+        if grid_rows[x-1][y] >= 0 and grid_rows[x-1][y] <= len(grid_rows[1]) and grid_rows[x-1][y] == 0:
+            stack.append([x-1, y])
+        if grid_rows[x][y+1] >= 0 and grid_rows[x][y+1] <= len(grid_rows) and grid_rows[x][y+1] == 0:
+            stack.append([x, y+1])
+        if grid_rows[x][y-1] >= 0 and grid_rows[x][y-1] <= len(grid_rows) and grid_rows[x][y-1] == 0:
+            stack.append([x, y-1])
+        if stack_pos + 1 in range(0,len(stack)):
+            stack_pos += 1
+        else:
+            last_visited = visited[len(visited)]
+            stack_pos = visited.index(last_visited)
+
 
 
 
@@ -51,24 +74,24 @@ grid_columns = [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
              [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
 
 
+while is_solvable(grid_rows) == False:
+    for row in range(len(grid_rows)):
+        for col in range(len(grid_rows[row])):
+            grid_rows[row][col] = random.randint(0, 1)
 
-for row in range(len(grid_rows)):
-    for col in range(len(grid_rows[row])):
-        grid_rows[row][col] = random.randint(0, 1)
+    for row in range(len(grid_rows)):
+        for col in range(len(grid_rows[row])):
+            if grid_rows[row][col] == 1 or grid_rows[row][col] == 2:
+                grid_rows[row][col] = random.randint(1,2)
 
-for row in range(len(grid_rows)):
-    for col in range(len(grid_rows[row])):
-        if grid_rows[row][col] == 1 or grid_rows[row][col] == 2:
-            grid_rows[row][col] = random.randint(1,2)
+    for column in range(len(grid_columns)):
+        for row in range(len(grid_columns[column])):
+            grid_columns[column][row] = random.randint(0, 1)
 
-for column in range(len(grid_columns)):
-    for row in range(len(grid_columns[column])):
-        grid_columns[column][row] = random.randint(0, 1)
-
-for column in range(len(grid_columns)):
-    for row in range(len(grid_columns[column])):
-        if grid_columns[column][row] == 1 or grid_columns[column][row] == 2:
-            grid_columns[column][row] = random.randint(1,2)
+    for column in range(len(grid_columns)):
+        for row in range(len(grid_columns[column])):
+            if grid_columns[column][row] == 1 or grid_columns[column][row] == 2:
+                grid_columns[column][row] = random.randint(1,2)
 
 t.speed(0)
 t.teleport(0,0)
