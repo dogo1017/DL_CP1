bdf4d389fb6780b91b58ca0d19d01b73bce01f19
dogo1017 <douglas.london@ucas-edu.net>
2025-11-19 19:14:40 +0000

    finished sqaured nums

--- practices/squared_numbers.py
+++ practices/squared_numbers.py
@@ -1,4 +1,17 @@
 #DL 1st, squared numbers practice
 
-numbers = [3, 7, 12, 25, 30, 45, 50, 65, 70, 85, 90, 105, 110, 125, 130, 145, 150, 165, 170, 185, 190, 205, 210, 225, 230, 245, 250, 265, 270, 285]
-print(f"\nOriginal: {numbers[1]}, Squared: ".join(list(map(lambda number: f"{number * number}", numbers))))
+#code golfed(72)
+r=range(21);l=list(map(lambda n:n*n,r))
+for i in r:print(f"{i}²={l[i]}")
+
+
+# One line version(75)
+#r=range(21);l=list(map(lambda n:n*n,r));[print(f"{i}^2={l[i]}") for i in r]
+
+
+# version with better output if first one doesnt meet rubric
+#r=range(21);l=list(map(lambda n:n*n,r))
+#for i in r:print(f"Original: {i}, Squared:{l[i]}")
+
+# Non map Version(40)
+#for i in range(21):print(f"{i}²={i*i}")
