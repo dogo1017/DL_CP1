905e2623e16f91144f6cf3bf2812bd297921d200
dogo1017 <douglas.london@ucas-edu.net>
2025-11-19 19:18:19 +0000

    pseudocode

--- practices/squared_numbers.py
+++ practices/squared_numbers.py
@@ -1,9 +1,9 @@
 #DL 1st, squared numbers practice
 
 #code golfed(72)
+# set r to range to show how many number, set l to the results of each number times itself using mapand lambda, then loop for each number in the list r that prints an output with the valuse from the current loop and the squared number using the current iteration as the index
 r=range(21);l=list(map(lambda n:n*n,r))
 for i in r:print(f"{i}²={l[i]}")
-
 
 # One line version(75)
 #r=range(21);l=list(map(lambda n:n*n,r));[print(f"{i}^2={l[i]}") for i in r]
