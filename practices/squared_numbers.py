8656305719c75796c5d645eab6aa973f20314705
dogo1017 <douglas.london@ucas-edu.net>
2025-11-20 09:52:50 -0700

    panda stuff

--- practices/squared_numbers.py
+++ practices/squared_numbers.py
@@ -9,9 +9,5 @@ for i in r:print(f"{i}²={l[i]}")
 #r=range(21);l=list(map(lambda n:n*n,r));[print(f"{i}^2={l[i]}") for i in r]
 
 
-# version with better output if first one doesnt meet rubric
-#r=range(21);l=list(map(lambda n:n*n,r))
-#for i in r:print(f"Original: {i}, Squared:{l[i]}")
-
 # Non map Version(40)
 #for i in range(21):print(f"{i}²={i*i}")
