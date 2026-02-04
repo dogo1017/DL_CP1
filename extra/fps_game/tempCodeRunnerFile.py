6982422fa3a090f8eba966ddcf0b64135642ccb6
dogo1017 <douglas.london@ucas-edu.net>
2026-02-04 09:52:54 -0700

    dino_game

---
+++ extra/fps_game/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+from direct.showbase.ShowBase import ShowBase
+from direct.actor.Actor import Actor
+from direct.task import Task
+from panda3d.core import Vec3, WindowProperties, ClockObject
+from math import sin, cos, pi
